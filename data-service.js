80ea37f4dfeaac3786ca2ae2a448f18966314c3b
Juhwan Kim <juhwankim159357@outlook.com>
2019-10-22 11:28:28 -0400

    setting a database

--- data-service.js
+++ data-service.js
@@ -1,13 +1,49 @@
 var ToDoList = new Array();
 var DoneList = new Array();
-
 var fs = require('fs');
+
+const Sequelize = require('sequelize');
+var sequelize = new Sequelize('dc4v31vvi4hcdn','kbpehfwtapmzoz','0c46c7c31aeca1defabe932df1f2318e93f679ff08de7180c76b8d47e15f3388',{
+    host:'ec2-184-73-209-230.compute-1.amazonaws.com',
+    dialect:'postgres',
+    port: 5432,
+    dialectOptions:{
+        ssl: true
+    }
+});
 var exports = module.exports = {};
+
+const ToDoList = sequelize.define('ToDoList', {
+    Listnum: {
+        type:Sequelize.INTEGER,
+        primaryKey:true,
+        autoIncrement:true
+    },
+    Listname:Sequelize.STRING,
+    Description:Sequelize.STRING
+});
+
+const DoneList = sequelize.define('DoneList',{
+    Donenum:{
+        type:Sequelize.INTEGER,
+        primaryKey:true,
+        autoIncrement:true
+    },
+    DoneName: Sequelize.STRING,
+    Description: Sequelize.STRING
+});
 
 exports.initialize = function() {
     
     return new Promise((resolve, reject) => {
-        fs.readFile('data/ToDoList.json', 'utf-8', (err, data) => {
+        sequelize.sync()
+        .then(() => resolve())
+        .catch(() => reject("unable to sync the database"));
+    });
+        
+        
+        
+        /*fs.readFile('data/ToDoList.json', 'utf-8', (err, data) => {
             if(err){
                 reject(new Error("Fail To init DO List file"));
             }
@@ -27,28 +63,24 @@ exports.initialize = function() {
 
         resolve("Successful Init");
     })
+    */
 };
 
 exports.getAllToDoList = function(){
     return new Promise((resolve, reject) => {
-        if(ToDoList.length != 0) {
-            resolve(ToDoList);
-    }
-        else {        
-            reject("No To Do List returned");
-    }
+        ToDoList.findAll()
+        .then(()=>resolve(ToDoList.findAll()))
+        .catch(()=>reject("No To Do List returned"))
     });
 };
 
 
 exports.getAllDoneList = function(){
     return new Promise((resolve, reject) => {
-        if (DoneList.length == 0)
-        reject("No DoneList returned");
-        else
-        resolve(DoneList);
+        DoneList.findAll()
+        .then(()=>resolve(DoneList.findAll()))
+        .catch(()=>reject("No DoneList returned"))
     });
-
 };  
 
 exports.addToDoList = function(ToDoListData) {
